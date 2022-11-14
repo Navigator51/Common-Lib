a3e4581902bd1d75614b47a192eacfd9938f7618
ShtooR <markov24@vk.com>
2022-11-14 21:11:02 +0300

    тесты последней апи

--- src/main/java/su/goodcat/commonlib/domain/CounterpartyDocumentRequestDTO.java
+++ src/main/java/su/goodcat/commonlib/domain/CounterpartyDocumentRequestDTO.java
@@ -1,8 +1,10 @@
 package su.goodcat.commonlib.domain;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
+@AllArgsConstructor
 public class CounterpartyDocumentRequestDTO {
     private FrontStatus frontStatus;
     private Status forbiddenStatus;
